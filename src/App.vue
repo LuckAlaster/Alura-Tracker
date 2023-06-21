61defe2779f4dbf5de3c9261fb53f1a045a93a34
Kevin <kevin22np@gmail.com>
2023-06-21 20:30:10 -0300

    Criação inicial do projeto Alura Tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="columns is-gapless is multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+
+    <div class="column is-tree-quarter">
+      <Formulario />
+    </div>
+    
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+export default defineComponent({
+  components: { 
+    BarraLateral, 
+    Formulario 
+  },
+  name: "app",
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
